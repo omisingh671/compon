02bed51d3813cc3243ff669b605f6dc92035c32d
Amarjeet Singh <omisingh671@gmail.com>
2021-09-02 02:22:35 +0530

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // AOS ANIMATIONS
+    AOS.init();
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+    
+  })(window.jQuery);
+
+  
