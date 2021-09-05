3dba7462601014855c7c07db4ce6cfd864f18e70
Amarjeet Singh <omisingh671@gmail.com>
2021-09-05 21:52:40 +0530

    HTML Pages and Navbar Update

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,43 @@
     $('.navbar-nav .nav-link').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
+
+    let pathParts = window.location.pathname.split('/');
+    let pathNames = pathParts[2].split('.');
+    let pathName = pathNames[0];
+
+    $("#navbarNav ul li.active").removeClass('active');
+
+    switch(pathName){
+      case 'index':
+        $("#navbarNav ul li a:contains('Home')").parent().addClass('active');
+        console.log(pathName);
+        break;
+      case 'about':
+        $("#navbarNav ul li a:contains('About Us')").parent().addClass('active');
+        console.log(pathName);
+        break;
+      case 'publications':
+        $("#navbarNav ul li a:contains('Publications')").parent().addClass('active');
+        console.log(pathName);
+        break;
+      case 'case-teams':
+        $("#navbarNav ul li a:contains('Case Teams')").parent().addClass('active');
+        console.log(pathName);
+        break;
+      case 'contact':
+        $("#navbarNav ul li a:contains('Contact us')").parent().addClass('active');
+        console.log(pathName);
+        break;
+      default:
+        $("#navbarNav ul li a:contains('Home')").parent().addClass('active');
+        break;
+    }
+
+    // $("#navbarNav ul li.active").removeClass('active');
+
+    // $("#navbarNav ul li a:contains('Home')").parent().addClass('active');
+    // $("#navbarNav ul li a:contains('About Us')").parent().addClass('active');
     
   })(window.jQuery);
 
